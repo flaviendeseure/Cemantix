a045361ed475f6cae4c541aacc697fd50e07d884
Flavien Deseure--Charron <flavien.deseure@gmail.com>
2022-10-25 14:30:57 +0200

    Similarity

--- index.ts
+++ index.ts
@@ -1,12 +1,29 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response(similarity("World", "World"));
+  return new Response(similarity("World", "World").toString());
 }
 
 serve(handler);
 
-function similarity(a: string, b:string){
-  return a + b;
+async function similarity(word1: string, word2:string){
+  const body = {
+    sim1: word1,
+    sim2: word2,
+    lang: "fr",
+    type: "General Word2Vec",
+  };
+  const similarityResponse = await fetch(
+    "http://nlp.polytechnique.fr/similarityscore",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }
+  );
+  const similarityResponseJson = await similarityResponse.json();
+  return Number(similarityResponseJson.simscore);
 }
 
