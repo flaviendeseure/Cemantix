69cbac92b3b66767423d39e1bb3449c5613c7cbb
Flavien Deseure--Charron <flavien.deseure@gmail.com>
2022-10-25 14:45:43 +0200

    Similarity - call func

--- index.ts
+++ index.ts
@@ -1,26 +1,29 @@
-import {serve} from "https://deno.land/std@0.119.0/http/server.ts";
-
-const similarity = async (word1: string, word2: string): Promise<string> => {
-    const body = {
-        sim1: word1,
-        sim2: word2,
-        lang: "fr",
-        type: "General Word2Vec",
-    };
-    const similarityResponse = await fetch("http://nlp.polytechnique.fr/similarityscore", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-    });
-    const similarityResponseJson = await similarityResponse.json();
-    return similarityResponseJson.simscore;
-}
+import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-    return new Response(await similarity("hello", "world"));
+  return new Response(await String(similarity("chien", "chat"));
 }
 
 
+async function similarity(word1: string, word2:string){
+  const body = {
+    sim1: word1,
+    sim2: word2,
+    lang: "fr",
+    type: "General Word2Vec",
+  };
+  const similarityResponse = await fetch(
+    "http://nlp.polytechnique.fr/similarityscore",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }
+  );
+  const similarityResponseJson = await similarityResponse.json();
+  return Number(similarityResponseJson.simscore);
+}
 serve(handler);
+
