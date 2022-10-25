2feb59e0a901d526f80b2c6378293d850b2c3d72
Flavien Deseure--Charron <flavien.deseure@gmail.com>
2022-10-25 14:50:49 +0200

    Similarity - str

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response(await similarity("chien", "chat"));
+  return new Response(String(await similarity("chien", "chat")));
 }
 
 
@@ -23,7 +23,7 @@ async function similarity(word1: string, word2:string){
     }
   );
   const similarityResponseJson = await similarityResponse.json();
-  return similarityResponseJson.simscore;
+  return Number(similarityResponseJson.simscore);
 }
 
 serve(handler);
