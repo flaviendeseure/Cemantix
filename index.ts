418acc9b043b0c42ee476dae581f4750f52f4934
Flavien Deseure--Charron <flavien.deseure@gmail.com>
2022-10-25 14:27:09 +0200

    Init

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+  return new Response(similarity("World", "World"));
 }
 
 serve(handler);
+
+function similarity(a: string, b:string){
+  return a + b;
+}
+
